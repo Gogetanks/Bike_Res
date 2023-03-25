e2a378f5e81a96b0af8f4c313355abb55d0fbe56
Gogetanks <01155543@pw.edu.pl>
2023-03-25 12:25:29 +0100

    bike category page

--- bike/views.py
+++ bike/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
+
+
+def home(request):
     return HttpResponse("Hello, world!")
+
+
+def bikes(request):
+    return HttpResponse("Bikes")
