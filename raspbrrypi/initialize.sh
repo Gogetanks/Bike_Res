b985d39c9175143829cb78fb836072dafa3622c4
011557 <user1@s052d7bd1.fastvps-server.com>
2023-06-16 20:25:38 +0300

    password for linux update

--- raspbrrypi/initialize.sh
+++ raspbrrypi/initialize.sh
@@ -8,4 +8,6 @@ echo "Specify your SSID and password in file zero_w_bike/etc/wpa_supplicant.conf
 
 echo "Specify your openvpn sertificates in file zero_w_bike/etc/openvpn/client/openvpn-user-r.conf"
 
+echo "Specify your root password in file buildroot-2023.02/.config in line BR2_TARGET_GENERIC_ROOT_PASSWD"
+
 
